82700a745cbe88dca760e26a8e50f96967f11d87
tallon <talloner@qq.com>
2020-04-29 12:00:44 +0800

    添加测试用例

---
+++ commons/commons-utils/src/test/java/com/tallon/OkHttp3Test.java
@@ -0,0 +1,17 @@
+package com.tallon;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author tallon
+ * @version v1.0.0
+ * @date 2020-04-29 00:22
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class OkHttp3Test {
+
+
+}
