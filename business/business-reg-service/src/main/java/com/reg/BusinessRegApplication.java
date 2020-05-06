5aa12d6137bd8a1c62a3a911391b6a8e5f3b235d
tallon <talloner@qq.com>
2020-05-06 16:35:31 +0800

    常规开发

--- business/business-reg-service/src/main/java/com/reg/BusinessRegApplication.java
+++ business/business-reg-service/src/main/java/com/reg/BusinessRegApplication.java
@@ -2,6 +2,7 @@ package com.reg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * reg启动类
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020-04-29 15:14
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BusinessRegApplication {
     public static void main(String[] args) {
         SpringApplication.run(BusinessRegApplication.class, args);
